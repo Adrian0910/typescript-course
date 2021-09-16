69bb38697c5cef74658303aa5357dc38b124aa2a
Adrian0910 <uchihasantander@gmail.com>
2021-09-15 21:08:24 -0500

    Booleans

---
+++ bases/tipos/booleans.ts
@@ -0,0 +1,15 @@
+
+(() => {
+
+    let isSuperman:boolean = true;
+    let isBatman:boolean = false;
+
+    isSuperman = ( isBatman ) ? true: false;
+
+    console.log({ isSuperman });
+
+})()
+
+
+
+

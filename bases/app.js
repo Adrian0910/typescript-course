d529aa30ec4e576d96657f00b9524253094ee8f9
Adrian0910 <uchihasantander@gmail.com>
2021-09-15 20:58:15 -0500

    funcion anonima autoinvocada

--- bases/app.js
+++ bases/app.js
@@ -1,15 +1,7 @@
-
+"use strict";
 // "Funcion anonima autoinvicada"
 // crea varios archivos de mi proyecto sin caer en el scop global
-(() => {
-
-    const a:number = 10;
-
-    console.log(a)
-
-})()
-
-
-
-
-
+(function () {
+    var a = 10;
+    console.log(a);
+})();

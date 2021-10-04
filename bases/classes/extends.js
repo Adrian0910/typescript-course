e7779611d7da8c95c5cb6a49c2cc647150407fd3
Adrian0910 <uchihasantander@gmail.com>
2021-10-04 16:46:22 -0500

    extends

---
+++ bases/classes/extends.js
@@ -0,0 +1,45 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+(function () {
+    var Avenger = /** @class */ (function () {
+        function Avenger(name, realName) {
+            this.name = name;
+            this.realName = realName;
+            console.log('Constructor avenger llamado');
+        }
+        Avenger.prototype.getFullName = function () {
+            return this.name + " " + this.realName;
+        };
+        return Avenger;
+    }());
+    var Xmen = /** @class */ (function (_super) {
+        __extends(Xmen, _super);
+        function Xmen(name, realName, isMutant) {
+            var _this = _super.call(this, name, realName) || this;
+            _this.isMutant = isMutant;
+            console.log('Constructos xmen llamado');
+            return _this;
+        }
+        Xmen.prototype.getFullNameFromXmen = function () {
+            console.log(_super.prototype.getFullName.call(this));
+        };
+        return Xmen;
+    }(Avenger));
+    var wolverine = new Xmen('Wolverine', 'Logan', true);
+    console.log(wolverine);
+    wolverine.getFullNameFromXmen();
+})();
---
+++ bases/classes/extends.ts
@@ -0,0 +1,37 @@
+(() => {
+
+    class Avenger {
+        constructor(
+            public name: string, 
+            public realName: string,
+        ){
+            console.log('Constructor avenger llamado')
+        }
+
+        protected getFullName() {
+            return `${ this.name } ${this.realName}`
+        }
+    }
+
+    class Xmen extends Avenger {
+        constructor(
+            name: string,
+            realName: string,
+            public isMutant: boolean
+        ){
+            super( name, realName )
+            console.log('Constructos xmen llamado')
+        }
+
+        getFullNameFromXmen() {
+            console.log( super.getFullName() )
+        }
+    }
+
+    const wolverine = new Xmen('Wolverine', 'Logan', true)
+
+    console.log(wolverine);
+    wolverine.getFullNameFromXmen();
+
+
+})()
